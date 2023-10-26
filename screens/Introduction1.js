31da2f1c21d187bdb1f4ee30a5cd6496e878dab5
ReallyNguyen <jnguyen188@my.bcit.ca>
2023-10-25 23:35:35 -0700

    svg

---
+++ screens/Introduction1.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Next from '../components/button/Next';
+import { colors } from '../theme';
+import Logo from '../assets/logo/logo'
+
+export default function Introduction1({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <Text>Hi</Text>
+            <Logo />
+            <Next />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        justifyContent: 'flex-end',
+    },
+});
