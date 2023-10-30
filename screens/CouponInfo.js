aa810c291adf2ca987fe01c737c8d2e9792ec9cd
mpedrano <mpedrano@my.bcit.ca>
2023-10-30 12:58:37 -0700

    coupon update

---
+++ screens/CouponInfo.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Animated } from 'react-native';
+
+export default function CouponInfo({ navigation }) {
+    return(
+        <View>
+            <Text>Coupon Info</Text>
+        </View>
+    )
+}
