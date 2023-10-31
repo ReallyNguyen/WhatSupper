9f3a57f8e914bbe7a807fced39128b03addaae02
mpedrano <mpedrano@my.bcit.ca>
2023-10-30 23:07:31 -0700

    update recipes

---
+++ data/recipe.js
@@ -0,0 +1,32 @@
+export const recipe = [
+    {
+        "id": 0,
+        "img": require('../assets/recipes/friedrice.jpeg'),
+        "name": "Fried Rice",
+    },
+    {
+        "id": 1,
+        "img": require('../assets/recipes/hktoast.jpeg'),
+        "name": "Hong Kong Toast",
+    },
+    {
+        "id": 2,
+        "img": require('../assets/recipes/kfchicken.jpeg'),
+        "name": "Korean Fried Chicken",
+    },
+    {
+        "id": 3,
+        "img": require('../assets/recipes/pho.jpeg'),
+        "name": "Pho",
+    },
+    {
+        "id": 4,
+        "img": require('../assets/recipes/garlicsalmon.jpeg'),
+        "name": "Garlic Salmon",
+    },
+    {
+        "id": 5,
+        "img": require('../assets/recipes/hotpot.jpg'),
+        "name": "Hotpot",
+    },
+]
