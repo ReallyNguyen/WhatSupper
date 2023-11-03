e09b8c8ec3e74ba402f20d16dd0bd5d1fe9ff32e
ReallyNguyen <jnguyen188@my.bcit.ca>
2023-11-03 01:47:20 -0700

    navigation from recipe page to recipe info

--- screens/Recipe.js
+++ screens/Recipe.js
@@ -15,8 +15,8 @@ export default function Recipe({ navigation }) {
 
     if (isLoading) {
         return <View style={styles.loading}>
-                    <Text style={styles.loadingText}>Hold on, we are working our AI recipe magic! ✨</Text>
-                </View>
+            <Text style={styles.loadingText}>Hold on, we are working our AI recipe magic! ✨</Text>
+        </View>
     }
 
     return (
@@ -25,55 +25,58 @@ export default function Recipe({ navigation }) {
                 <Back navigation={navigation} />
             </View>
             <Text style={styles.heading}>Here are some recipes based on what you scanned 🪄</Text>
-            <Box
-                maxWidth="$72"
-                borderColor="$borderLight200"
-                borderRadius="$lg"
-                borderWidth="$1"
-                my="$4"
-                overflow="hidden"
-                sx={{
-                    "@base": {
-                        mx: "$5",
-                    },
-                    "@lg": {
-                        my: "0",
-                    },
-                    _dark: {
-                        bg: "$backgroundDark900",
-                        borderColor: "$borderDark800",
-                    },
-                }}
-            >
-                <Box style={{ position: 'relative' }}>
-                    <Image
-                        h={120}
-                        w={310}
-                        source={require('../assets/recipes/hotpot.jpg')}
-                        resizeMode="cover"
-                        alt="image"
-                    />
-                    <View style={{ position: 'absolute', bottom: 0, left: 0, width: '100%', height: '60%', backgroundColor: 'rgba(0,0,0,0.5)' }}>
-                        <View style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-                            <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 10 }}>
-                                <Text style={{ color: 'white', fontSize: 16, fontWeight: "bold" }}>Hot Pot</Text>
-                                <View style={{ display: 'flex', flexDirection: 'row' }}>
-                                    <View style={{ backgroundColor: 'rgba(0,0,0,0.5)', padding: 5, borderRadius: 5, marginRight: 5 }}>
-                                        <Text style={{ color: 'white', fontSize: 10 }}>40 mins</Text>
-                                    </View>
-                                    <View style={{ backgroundColor: 'rgba(0,0,0,0.5)', padding: 5, borderRadius: 5, marginRight: 5 }}>
-                                        <Text style={{ color: 'white', fontSize: 10 }}>560 kcals</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('RecipeInfo')}>
+                <Box
+                    maxWidth="$72"
+                    borderColor="$borderLight200"
+                    borderRadius="$lg"
+                    borderWidth="$1"
+                    my="$4"
+                    overflow="hidden"
+                    sx={{
+                        "@base": {
+                            mx: "$5",
+                        },
+                        "@lg": {
+                            my: "0",
+                        },
+                        _dark: {
+                            bg: "$backgroundDark900",
+                            borderColor: "$borderDark800",
+                        },
+                    }}
+                >
+                    <Box style={{ position: 'relative' }}>
+                        <Image
+                            h={120}
+                            w={310}
+                            source={require('../assets/recipes/hotpot.jpg')}
+                            resizeMode="cover"
+                            alt="image"
+                        />
+                        <View style={{ position: 'absolute', bottom: 0, left: 0, width: '100%', height: '60%', backgroundColor: 'rgba(0,0,0,0.5)' }}>
+                            <View style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+                                <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 10 }}>
+                                    <Text style={{ color: 'white', fontSize: 16, fontWeight: "bold" }}>Hot Pot</Text>
+                                    <View style={{ display: 'flex', flexDirection: 'row' }}>
+                                        <View style={{ backgroundColor: 'rgba(0,0,0,0.5)', padding: 5, borderRadius: 5, marginRight: 5 }}>
+                                            <Text style={{ color: 'white', fontSize: 10 }}>40 mins</Text>
+                                        </View>
+                                        <View style={{ backgroundColor: 'rgba(0,0,0,0.5)', padding: 5, borderRadius: 5, marginRight: 5 }}>
+                                            <Text style={{ color: 'white', fontSize: 10 }}>560 kcals</Text>
+                                        </View>
                                     </View>
                                 </View>
+                                <Text style={{ color: 'white', fontSize: 10, paddingHorizontal: 10, marginTop: 10 }}>
+                                    A thorough how-to guide to Chinese hot pot covering all aspects of preparing this iconic
+                                    meal at home....
+                                </Text>
                             </View>
-                            <Text style={{ color: 'white', fontSize: 10, paddingHorizontal: 10, marginTop: 10 }}>
-                                A thorough how-to guide to Chinese hot pot covering all aspects of preparing this iconic
-                                meal at home....
-                            </Text>
                         </View>
-                    </View>
+                    </Box>
                 </Box>
-            </Box>
+            </TouchableOpacity>
+
 
         </View>
     );
